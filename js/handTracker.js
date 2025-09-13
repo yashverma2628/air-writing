7ad3e76ccbf048ef27e5b29308bccf27ec96b314
yashverma2628 <gourishankarverma2628@gmail.com>
2025-09-13 16:27:34 +0530

    Create handTracker.js

---
+++ js/handTracker.js
@@ -0,0 +1,29 @@
+let hands;
+
+async function initialize(videoElement, onResultsCallback) {
+    hands = new Hands({
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+    });
+
+    hands.setOptions({
+        maxNumHands: 1,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5
+    });
+
+    hands.onResults(onResultsCallback);
+
+    const camera = new Camera(videoElement, {
+        onFrame: async () => {
+            await hands.send({ image: videoElement });
+        },
+        width: 1280,
+        height: 720
+    });
+
+    return camera.start();
+}
+
+export { initialize };
+
